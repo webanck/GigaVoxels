b514ffcaafc45a2969645bb816dc97e565b88a83
Antoine Webanck <antoine.webanck@gmail.com>
2015-04-16 12:59:14 -0400

    Script d'installation et compilation du module DynamicLoad.

--- Install/Linux/makeInstall.sh
+++ Install/Linux/makeInstall.sh
@@ -4,6 +4,8 @@
 # FULL INSTALLATION
 # **************************************************************************
 
+CURRENT_PATH=$PWD
+
 #Selection of the QT version for systems with multiple versions.
 export QT_SELECT=4
 
@@ -20,3 +22,16 @@ sh ./makeViewerPluginTutorials.sh
 sh ./updateData.sh
 sh ./updateRelease.sh
 sh ./updateShaders.sh
+
+#Compilation of the library and the tools.
+cd ../../Generated_Linux/Library
+make
+cd $CURRENT_PATH
+cd ../../Generated_Linux/Tools
+make
+cd $CURRENT_PATH
+
+#Compilation of the DynamicLoad ViewerPlugin.
+cd ../../Generated_Linux/Tutorials/ViewerPlugins
+make GvDynamicLoad
+cd $CURRENT_PATH
