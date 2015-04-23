a887bcdd974f06f6bc64a74265f0da1adc7c072d
Antoine Webanck <antoine.webanck@gmail.com>
2015-04-23 18:18:31 -0400

    Le script d'installation complète regénère maintenant les binaires et s'exécute dans un ordre plus juste.

--- Install/Linux/makeInstall.sh
+++ Install/Linux/makeInstall.sh
@@ -9,29 +9,33 @@ CURRENT_PATH=$PWD
 #Selection of the QT version for systems with multiple versions.
 export QT_SELECT=4
 
-#Creation of the compilation directory.
-sh ./makeDemoTutorials.sh
-sh ./makeDocumentation.sh
-sh ./makeLibrary.sh
-sh ./makeRSACosmosTutorials.sh
-sh ./makeTests.sh
-sh ./makeTools.sh
-sh ./makeViewerPluginTutorials.sh
-
 #Copy of the required stuff for the binaries directory.
-sh ./updateData.sh
-sh ./updateRelease.sh
-sh ./updateShaders.sh
+sh ./updateData.sh && \
+sh ./updateShaders.sh && \
 
-#Compilation of the library and the tools.
-cd ../../Generated_Linux/Library
-make
-cd $CURRENT_PATH
-cd ../../Generated_Linux/Tools
-make
-cd $CURRENT_PATH
+#Compilation of the library.
+sh ./makeLibrary.sh && \
+cd ../../Generated_Linux/Library && \
+make clean && \
+make && \
+cd $CURRENT_PATH && \
+
+#Compilation of the tools.
+sh ./makeTools.sh && \
+cd ../../Generated_Linux/Tools && \
+make clean && \
+make && \
+cd $CURRENT_PATH && \
 
 #Compilation of the DynamicLoad ViewerPlugin.
-cd ../../Generated_Linux/Tutorials/ViewerPlugins
-make GvDynamicLoad
+sh ./makeDemoTutorials.sh && \
+sh ./makeViewerPluginTutorials.sh && \
+cd ../../Generated_Linux/Tutorials/ViewerPlugins && \
+make clean && \
+make GvDynamicLoad && \
 cd $CURRENT_PATH
+
+#~ sh ./updateRelease.sh
+#~ sh ./makeDocumentation.sh
+#~ sh ./makeRSACosmosTutorials.sh
+#~ sh ./makeTests.sh
